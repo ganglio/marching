15a6575d5d1a1e65f14944e06cdb5785ebe9f51f
Roberto Torella <robertot@eu.square-enix.com>
2021-12-10 15:27:45 +0000

    Added geometries projecting function

--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"marching/drawer"
+	"marching/world"
 
 	"github.com/gonutz/prototype/draw"
 )
 
 func main() {
-	draw.RunWindow("Title", 1024, 768, drawer.Update(nil))
+	w := world.NewWorld(world.Circle(0, 0, 2))
+
+	draw.RunWindow("Title", 1024, 768, drawer.Update(w))
 }
